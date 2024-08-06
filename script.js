ca01e3c69b9d4ea975819a7da914adfc4c9b8a0b
Lucas Benkendorf <benklucass@gmail.com>
2024-08-06 19:09:29 -0300

    exercicio 7 trello

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function soma() {
     var resultado = 5 + 8;
     alert(resultado);
 }
+
 function capturarValores() {
   
     var inputNumero1 = document.getElementById('numero1');
@@ -12,3 +13,17 @@ function capturarValores() {
 
     console.log('Valor do Número 1:', valorNumero1);
     console.log('Valor do Número 2:', valorNumero2);}
+
+    function capturarValores() {
+        var inputNumero1 = document.getElementById('numero1');
+        var inputNumero2 = document.getElementById('numero2');
+    
+        var valorNumero1 = parseFloat(inputNumero1.value);
+        var valorNumero2 = parseFloat(inputNumero2.value);
+    
+        var soma = valorNumero1 + valorNumero2;
+    
+        console.log('Valor do Número 1:', valorNumero1);
+        console.log('Valor do Número 2:', valorNumero2);
+        console.log('Resultado da soma:', soma);
+    }
