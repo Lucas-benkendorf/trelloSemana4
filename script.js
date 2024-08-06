0bdc267b1230b7f090e9873743f8df997deb1da8
Lucas Benkendorf <benklucass@gmail.com>
2024-08-06 19:04:44 -0300

    exercicio 6 trello

---
+++ script.js
@@ -0,0 +1,14 @@
+function soma() {
+    var resultado = 5 + 8;
+    alert(resultado);
+}
+function capturarValores() {
+  
+    var inputNumero1 = document.getElementById('numero1');
+    var inputNumero2 = document.getElementById('numero2');
+
+    var valorNumero1 = inputNumero1.value;
+    var valorNumero2 = inputNumero2.value;
+
+    console.log('Valor do Número 1:', valorNumero1);
+    console.log('Valor do Número 2:', valorNumero2);}
